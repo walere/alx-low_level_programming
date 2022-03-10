d043b3cef343caf4e195dd839966311a10186423
walere <akinwaleredaniel.dev@gmail.com>
2022-03-10 15:29:01 -0800

    6 edited

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 /**
  * main - program that prints the size of various data types
  * Use the printf function
@@ -12,10 +13,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("size of a char: %d byte(s)\n", sizeof(c));
-	printf("size of an int: %d byte(s)\n", sizeof(i));
-	printf("size of a long int: %d byte(s)\n", sizeof(li));
-	printf("size of a long long: %d byte(s)\n", sizeof(lli));
-	printf("size of a float: %d byte(s)\n", sizeof(f));
+	printf("size of a char: %zu byte(s)\n", sizeof(c));
+	printf("size of an int: %zu byte(s)\n", sizeof(i));
+	printf("size of a long int: %zu byte(s)\n", sizeof(li));
+	printf("size of a long long: %zu byte(s)\n", sizeof(lli));
+	printf("size of a float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
