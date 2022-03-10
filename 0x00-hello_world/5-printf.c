1d8cb6bdd8daae07b87953b6f50fc2d483246b80
walere <akinwaleredaniel.dev@gmail.com>
2022-03-10 12:27:25 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,8 @@
+/** A C PROGRAM THAT PRINTS 'WITH PROPER GRAMMER,BUT THE OUTCOME IS A PIECE OF ART'*/
+#include <stdio.h>
+
+int main ()
+{
+	printf("with proper grammer,but the outcome is a piece of art,\n");
+	return 0;
+}	
