7c08226c07f5e808111a9445394a5492e40cb121
walere <akinwaleredaniel.dev@gmail.com>
2022-03-11 16:49:25 -0800

    1-5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - a program that prints all single digit numbers
+ * followed by a new line
+ * Return: return 0 if code runs correctly.
+ */
+
+int main(void)
+{
+	int numb;
+
+	for(numb = 0 ; numb <= 10 ; numb++)
+	{
+		printf("%d \n", numb);
+	}
+	return (0);
+}
