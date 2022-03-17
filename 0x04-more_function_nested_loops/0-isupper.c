27523c9b14dce38a7a1537a0016f5e34dc251488
walere <akinwaleredaniel.dev@gmail.com>
2022-03-17 11:02:20 -0700

    first commit

---
+++ 0x04-more_function_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_isupper: checks if the character is in uppercase
+ *@c : this is the variable
+ *Return: 1 if c is  upper case
+ *otherwise return 0
+ */
+int _isupper(int c)
+{
+	int c
+	
+	if (c => 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+}
---
+++ 0x04-more_function_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains more solution to the alx function task, so far the program has been for a month and i have learnt a lot.
