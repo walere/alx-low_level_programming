5d5737bc6b190c1cf72ee3c641e0013ef4dcedfd
walere <akinwaleredaniel.dev@gmail.com>
2022-03-21 12:10:51 -0700

    fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
  * reset_to_98 - a function that updates a pointers value to 98
  * @n: This is the argument of the function
@@ -10,7 +8,7 @@
 void reset_to_98(int *n)
 {
 	int *n;
-	
+
 	*n = 98;
 	return (0);
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,2 @@
-#ifndef FILE_HOLBERTON
-#define FILE_HOLBERTON
-
 int _putchar(char c);
 void reset_to_98(int *n);
-
-
-#endif
